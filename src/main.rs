65d12dcaaf83ea1429e1ee1023e680cc620e75eb
Jan Gosmann <jan@hyper-world.de>
2022-12-29 16:58:51 +0100

    Configure HTTP request timeout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{fs::File, time::Duration};
 
 use clap::Parser;
 use serde::Deserialize;
@@ -10,8 +10,13 @@ use providers::Provider;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
 struct Args {
+    /// Configuration file to load
     #[arg(long = "config.file")]
     config: std::path::PathBuf,
+
+    /// Timeout for HTTP requests (seconds)
+    #[arg(long = "http.timout", default_value_t = 10)]
+    http_timeout_seconds: u64,
 }
 
 #[derive(Deserialize)]
@@ -29,6 +34,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let http_client = reqwest::Client::builder()
         .user_agent(APP_USER_AGENT)
+        .timeout(Duration::from_secs(args.http_timeout_seconds))
         .build()?;
 
     for provider in config.providers {
